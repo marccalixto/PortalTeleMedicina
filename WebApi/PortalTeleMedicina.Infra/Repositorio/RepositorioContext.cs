48d3eb85ce4d82a00816e24ae8fd91fbcd0e59e2
Marcelo Calixto <marccalixto@hotmail.com>
2021-06-26 18:15:40 -0400

    Envio projeto

---
+++ WebApi/PortalTeleMedicina.Infra/Repositorio/RepositorioContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PortalTeleMedicina.Infra.Repositorio
+{
+    public class RepositorioContext : DbContext
+    {
+        public RepositorioContext(DbContextOptions<RepositorioContext> options) : base(options)
+        { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseLazyLoadingProxies();
+                optionsBuilder.UseSqlServer();
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.ApplyConfiguration(new PessoaConfiguration());
+            //modelBuilder.ApplyConfiguration(new DependenteConfiguration());
+
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ WebApi/PortalTeleMedicina.WebAPI/ViewModels/UsuarioSignupVM.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace PortalTeleMedicina.WebAPI.ViewModels
+{
+    public class UsuarioSignupVM : UsuarioSigninVM
+    {
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+    }
+}
